6c474dde153becfe29a67458188d300cc6eceb2c
Myriam <myriam.cote.4@gmail.com>
2019-10-19 22:30:28 +0200

    sparql cimetery query + result display code amend

--- module1/sp.py
+++ module1/sp.py
@@ -4,7 +4,7 @@ import argparse
 from datetime import datetime as dt
 import sys
 
-from SPARQLWrapper import SPARQLWrapper, JSON 
+from SPARQLWrapper import SPARQLWrapper, JSON
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--filter', type=str, help='Filtering on name')
@@ -15,15 +15,14 @@ def get_rows():
     sparql = SPARQLWrapper(endpoint)
 
     statement = """
-    SELECT DISTINCT ?item ?itemLabel ?place ?placeLabel WHERE {
-        ?place wdt:P17 wd:Q31;
-          wdt:P31 wd:Q39614;
-          wdt:P131 ?item.
-        ?item wdt:P131 wd:Q90870.
-
-        SERVICE wikibase:label { bd:serviceParam wikibase:language "en","fr". }
+    SELECT DISTINCT ?person ?personLabel ?dateBirth ?dateDeath WHERE {
+        ?person wdt:P27 wd:Q31 .
+        ?person wdt:P106 wd:Q82955 .
+        ?person wdt:P569 ?dateBirth .
+        ?person wdt:P570 ?dateDeath .
+        SERVICE wikibase:label { bd:serviceParam wikibase:language "en" . }
     }
-order by ?itemLabel
+    ORDER BY ?personLabel
     """
 
     sparql.setQuery(statement)
@@ -31,21 +30,27 @@ order by ?itemLabel
     results = sparql.query().convert()
 
     rows = results['results']['bindings']
-    print(f"\n{len(rows)} Brussels cimeterys found\n")
+    print(f"\n{len(rows)} Belgian politicians found\n")
     return rows
 
 def show(rows, filter=None, n=10):
-    """Display n cemeterys (default=10)"""
+    """Display n politicians (default=10)"""
     date_format = "%Y-%m-%dT%H:%M:%SZ"
     if filter:
-        rows = [row for row in rows if filter in row['placeLabel']['value'].lower()]
+        rows = [row for row in rows if filter in row['personLabel']['value'].lower()]
     print(f"Displaying the first {n}:\n")
     for row in rows[:n]:
         try:
-            commune= row['itemLabel']['value']
+            birth_date = dt.strptime(row['dateBirth']['value'], date_format)
+            birth_year = birth_date.year
         except ValueError:
-            commune = "????"
-        print(f"{row['placeLabel']['value']} ({commune})")
+            birth_year = "????"
+        try:
+            death_date = dt.strptime(row['dateDeath']['value'], date_format)
+            death_year = death_date.year
+        except ValueError:
+            death_year = "????"
+        print(f"{row['personLabel']['value']} ({birth_year}-{death_year})")
 
 if __name__ == "__main__":
     args = parser.parse_args()
