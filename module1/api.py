ade21bb73db5e0e64b5e9a030ad868cf7fb4f3cf
Myriam <myriam.cote.4@gmail.com>
2019-10-21 21:03:28 +0200

    change code for gender by name api

--- module1/api.py
+++ module1/api.py
@@ -4,56 +4,32 @@ import json
 import requests
 import sys #
 
-def print_coord(address):
-    """Retrieve coordinates from Open Street Map"""
-    osm = "https://nominatim.openstreetmap.org/search"
-    data = {'q': address, 'format': 'json'} #pour éviter bruit css si xml-html
-    resp = requests.get(osm, data)
-    json_list = json.loads(resp.text)
-    for item in json_list:
-        display_name = item['display_name']
-        short_name = display_name.split(", ")[0]
-        lat = item['lat']
-        lon = item['lon']
-        print(f"{short_name} ({lat} - {lon})")
-
-def print_info(country_name):
-    """Retrieve country info from REST API"""
-    base_url = "https://restcountries.eu/rest/v2/"
+def print_info(person_name):
+    """Retrieve gender by name  from GENDER BY NAME API"""
+    base_url = "https://udayogra-find-gender-by-name-v2/"
     name_url = base_url + "name/"
-    code_url = base_url + "alpha/"
-    resp = requests.get(name_url + country_name)
+    code_url = base_url + "analysis/"
+    resp = requests.get(name_url + person_name)
     try:
-        country = json.loads(resp.text)[0]
-        languages = country['languages']
-        print(f"Languages: {', '.join([lang['name'] for lang in languages])}")
-        border_codes = country['borders']
+        pname = json.loads(resp.text)[0]
+        female = pname['female']
+        print(f"Female: {', '.join([lang['name'] for lang in female])}")
+        male = pname['male']
         border_names = []
-        for code in border_codes:
-            resp = requests.get(code_url + code)
-            border_country = json.loads(resp.text)
-            border_name = border_country["name"]
-            border_names.append(border_name)
-        print(f"Borders: {', '.join(border_names)}")
+        print(f"Female: {', '.join([lang['name'] for lang in male])}")
     except KeyError:
-        print("Unknown country, please use English or native name")
+        print("Unknown name please use first name")
 
 if __name__ == "__main__":
     try:
         service = sys.argv[1]
-        if service == "coord":
+        if service == "gender":
             try:
-                address = sys.argv[2]
-                print_coord(address)
+                person_name = sys.argv[2]
+                print_info(person_name)
             except IndexError:
-                print("Please enter an address")
-        elif service == "info":
-            try:
-                country_name = sys.argv[2]
-                print_info(country_name)
-            except IndexError:
-                print("Please enter a country name")
+                print("Please enter a person name")
         else:
-            print("Unknown action, please use either 'coord' or 'info'")
+            print("Unknown action, please use 'gender'")
     except IndexError:
-        print("Missing action, please use either 'coord' or 'info'")
+        print("Missing action, please use either 'gender'")
