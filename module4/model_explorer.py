ed051d7ba3bac31de418e1ec9e2af7d754b4b947
Myriam <myriam.cote.4@gmail.com>
2019-11-24 15:37:57 +0100

    word similar functions changed

--- module4/model_explorer.py
+++ module4/model_explorer.py
@@ -6,24 +6,26 @@ from pprint import pprint
 
 model = Word2Vec.load("data/bulletins.model")
 
-word1 = "boucher"
+word1 = "théâtre"
 pprint(model.wv[word1])
 
 #%%
-word2 = "fleuriste"
+word2 = "église"
 sim1 = model.wv.similarity(word1, word2)
 print(f"{word1} is {100*sim1:.1f}% similar to {word2}\n")
 
 #%%
-word2 = "boulanger"
+word2 = "cimetière"
 sim1 = model.wv.similarity(word1, word2)
 print(f"{word1} is {100*sim1:.1f}% similar to {word2}\n")
 
 #%%
-pprint(model.wv.most_similar("kermesse", topn=3))
+pprint(model.wv.most_similar("école", topn=15))
 
 #%%
-pprint(model.wv.most_similar("bruxelles"))
+pprint(model.wv.most_similar("hôpital",topn=10))
 
 #%%
-pprint(model.wv.most_similar(positive=['bruxelles', 'france'], negative=['belgique']))
+pprint(model.wv.most_similar(positive=['mathématiques', 'université'], negative=['athénée'],topn=20))
+
+# %%
